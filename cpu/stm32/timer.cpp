f07099a80db27dd92544b47d93a920c81c4b2d7e
doctorbelka <avdbor.junior@mail.ru>
2026-05-12 11:10:08 +0300

    Timer

--- cpu/stm32/timer.cpp
+++ cpu/stm32/timer.cpp
@@ -2,7 +2,7 @@
 
 namespace stm32 {
 // Static instance tracking for interrupt routing
-static Timer* timerInstances_[17] = { nullptr };
+static Timer* timerInstances_[18] = { nullptr };
 
 // int TimerStm32::getTimerIndex(TIM_TypeDef* instance) {
 //     if (instance == TIM8) return 0;
@@ -66,7 +66,7 @@ void Timer::start()
 
 void Timer::stop()
 {
-    if (isOpen() || htim_ == nullptr)
+    if (!isOpen() || htim_ == nullptr)
         return;
 
     if (mode_ == Mode::OnePulse) {
@@ -95,8 +95,33 @@ bool Timer::ioctl(uint32_t cmd, void* pValue)
     return false;
 }
 
-void Timer::irqHandler(TIM_HandleTypeDef* htim)
+  void Timer::irqHandler(TIM_HandleTypeDef* htim)
 {
+    if (htim == nullptr)
+        return;
+
+    Timer* timer = nullptr;
+
+    if (htim->Instance == TIM8) {
+        timer = timerInstances_[8];
+    }
+    else if (htim->Instance == TIM16) {
+        timer = timerInstances_[16];
+    }
+    else if (htim->Instance == TIM17) {
+        timer = timerInstances_[17];
+    }
+
+    if (timer == nullptr)
+        return;
+
+    if (timer->mode_ == Mode::OnePulse) {
+        timer->generalCb().call_if();
+    }
+    else {
+        timer->captureCb().call_if();
+    }
+}
 //     int idx = getTimerIndex(htim->Instance);
 //     if (idx < 0 || instances_[idx] == nullptr)
 //         return;
@@ -114,3 +139,4 @@ void Timer::irqHandler(TIM_HandleTypeDef* htim)
 // }
 
 }             // ДОДЕЛАТЬ ПРЕРЫВАНИЕ
+
